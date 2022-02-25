6adf82f8fe250eec320c666ed8dec5ef14444f02
Jordan Farelli <jordanfarelli@gmail.com>
2022-02-24 20:03:33 -0700

    Added annotation

--- variables.js
+++ variables.js
@@ -1,3 +1,6 @@
+//Types of variables for string data type
 String: Alpha, Numeric, or Symbol
+//Types of numbers that are defined by the data type
 Numbers: 23, -23, 2.3
+//Boolean values are either true, or false
 Boolean: true and false
